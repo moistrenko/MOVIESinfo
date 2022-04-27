b458f6cf9afef51e2d56056944e4ec85c52c3bd1
Anatoly Moistrenko <a.moistrenko@carbonfay.ru>
2022-04-27 13:38:00 +0300

    added footer component

---
+++ src/components/AppContent/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "components/Header";
+import Footer from "components/Footer";
+
+import "./style.sass";
+
+function AppContent() {
+	return (
+		<div className="content">
+			<Header />
+			<main className='main'>asd</main>
+			<Footer />
+		</div>
+	);
+}
+
+export default AppContent;
