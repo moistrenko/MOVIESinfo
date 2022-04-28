db44759caf23ef3a5deb0d104dfc99026f4e3526
Anatoly Moistrenko <a.moistrenko@carbonfay.ru>
2022-04-28 11:19:03 +0300

    added header and fix footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
+import { routeMain as routerMainPage } from "pages/MainPage";
 import Logo from "components/Logo";
 
 import "./style.sass";
@@ -7,15 +9,16 @@ import "./style.sass";
 function Footer() {
 	return (
 		<footer className='footer wrapper'>
-			<div className="footer__wrapper">
-			<Logo />
-			<h2 className='footer__title'>Дипломный проект</h2>
-			<div className="author">
-				<div className="author__title">Made by</div>
-				<div className="author__name">Анатолий Мойстренко</div>
+			<div className='footer__wrapper'>
+				<NavLink to={routerMainPage()}>
+					<Logo />
+				</NavLink>
+				<h2 className='footer__title'>Дипломный проект</h2>
+				<div className='author'>
+					<div className='author__title'>Made by</div>
+					<div className='author__name'>Анатолий Мойстренко</div>
+				</div>
 			</div>
-			</div>
-			
 		</footer>
 	);
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import './style.sass';
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import { routeMain as routerMainPage } from "pages/MainPage";
+import { routeMain as routerCategoryPage } from "pages/MovieCategoryPage";
+import { routeMain as routerAboutPage } from "pages/AboutPage";
+import { routeMain as routerSearchPage } from "pages/SearchPage";
+
+import Logo from "components/Logo";
+
+import "./style.sass";
 
 function Header() {
-  return (
-    <header className="header">
-		header
-    </header>
-  );
+	return (
+		<header className='header wrapper'>
+			<div className='header__wrapper'>
+				<NavLink to={routerMainPage()}>
+					<Logo />
+				</NavLink>
+
+				<nav>
+					<NavLink className='header__link' to={routerMainPage()}>
+						Главная
+					</NavLink>
+					<NavLink className='header__link' to={routerCategoryPage()}>
+						Фильмы по категории
+					</NavLink>
+					<NavLink className='header__link' to={routerAboutPage()}>
+						О нас
+					</NavLink>
+					<NavLink className='header__link' to={routerSearchPage()}>
+						Поиск
+					</NavLink>
+				</nav>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
